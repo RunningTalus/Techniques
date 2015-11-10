55284dea6a0c56e317fff167758521731a6cc30b
Dave May <dave@davemay.org>
2015-11-10 11:24:28 -0700

    Added in comments.

--- 064-QuickServersForStaticSites/Gruntfile.js
+++ 064-QuickServersForStaticSites/Gruntfile.js
@@ -1,16 +1,18 @@
-module.exports = function(grunt) {
+module.exports = function(grunt) {  // All Gruntfiles export a function
+    grunt.loadNpmTasks('grunt-contrib-connect'); // Loads the Connect plug-in
+
     grunt.initConfig({
         connect: {
             server: {
                 options: {
                     port: 8080,
-                    base: 'public',
+                    base: 'public', // The base path for static files
                     keepalive: true
                 }
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.registerTask('default', ['connect:server']);
+
+    grunt.registerTask('default', ['connect:server']);  // Default command is aliased here
 };
